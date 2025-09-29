42cba668cfa662f6491525b3eff1d84cc28026ce
shivshankar-source <shivshakti.ss771@gmail.com>
2025-09-29 13:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let p = document.getElementById("timer");
+      setInterval(() => {
+        let date = new Date();
+
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let day = date.getDate();
+	
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let sec = date.getSeconds();
+		let ampm=hour>=12?'PM':'AM';
+		p.innerHTML = `${month + 1}/${ day}/${ year} ,${ hour}:${ min}:${ sec} ${ampm}`;}
+      , 1000)
